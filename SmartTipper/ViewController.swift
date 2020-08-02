3f3c95801915b78bf1b255e057631e30caba1392
Drew Beckmen <drew.beckmen@yale.edu>
2020-08-02 17:43:48 -0400

    first working version

--- SmartTipper/ViewController.swift
+++ SmartTipper/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Outlets: elements of screen you want to configure
+    //allows you to access the view element from the storyboard
+    //convention: end with type of view (Label, Control, etc.)
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipPercentageLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    //Everything originates from a user action
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        //already attached "Value Changed" event from segmented control
+        // ?? operator will assign bill to 0 if casting is unsuccessful
+        let bill = Double(billAmountTextField.text!) ?? 0
+        let tipPercentages = [0.15, 0.18, 0.2]
+        
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        //update labels with newly calculated values
+        tipPercentageLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 }
 
