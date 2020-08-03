532159e5285735868a17697a2e949e24f9b62465
Drew Beckmen <drew.beckmen@yale.edu>
2020-08-02 21:27:54 -0400

    dark mode, keyboard is first responder, round to next dollar, default tip amount

--- SmartTipper/SettingsViewController.swift
+++ SmartTipper/SettingsViewController.swift
@@ -10,13 +10,62 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet weak var defaultTipControl: UISegmentedControl!
+    @IBOutlet weak var roundSwitch: UISwitch!
+    @IBOutlet weak var darkModeSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        defaultTipControl.selectedSegmentIndex = UISegmentedControl.noSegment
+        
+        let defaults = UserDefaults.standard
+        let isRounding = defaults.bool(forKey: "roundUp")
+        let darkMode = defaults.bool(forKey: "darkMode")
+        if isRounding {
+            roundSwitch.isOn = true
+        }
+        else {
+            roundSwitch.isOn = false
+        }
+        
+        if darkMode {
+            overrideUserInterfaceStyle = .dark
+            darkModeSwitch.isOn = true
+        }
+        else {
+            overrideUserInterfaceStyle = .light
+            darkModeSwitch.isOn = false
+        }
     }
     
-
+    @IBAction func setDefaultTip(_ sender: Any) {
+        let tipAmounts = [0.15, 0.18, 0.2]
+        let currentIndex = defaultTipControl.selectedSegmentIndex
+        print(currentIndex)
+        let defaults = UserDefaults.standard
+        defaults.set(tipAmounts[currentIndex], forKey: "defaultTip")
+        defaults.synchronize()
+    }
+    
+    @IBAction func changeRoundUp(_ sender: Any) {
+        let defaults = UserDefaults.standard
+        defaults.set(roundSwitch.isOn, forKey: "roundUp")
+        defaults.synchronize()
+    }
+    
+    @IBAction func toggleDarkMode(_ sender: Any) {
+        let defaults = UserDefaults.standard
+        if darkModeSwitch.isOn {
+            overrideUserInterfaceStyle = .dark
+        }
+        else {
+            overrideUserInterfaceStyle = .light
+        }
+        defaults.set(darkModeSwitch.isOn, forKey: "darkMode")
+        defaults.synchronize()
+    }
     /*
     // MARK: - Navigation
 
--- SmartTipper/ViewController.swift
+++ SmartTipper/ViewController.swift
@@ -22,6 +22,39 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let defaults = UserDefaults.standard
+        let defaultTipSelected = defaults.double(forKey: "defaultTip")
+        let percentages = [0.15, 0.18, 0.2]
+        let selectedIndex = percentages.firstIndex(of: defaultTipSelected) ?? 0
+        tipControl.selectedSegmentIndex = selectedIndex
+        billAmountTextField.becomeFirstResponder()
+
+        let darkMode = defaults.bool(forKey: "darkMode")
+        
+        if darkMode {
+            overrideUserInterfaceStyle = .dark
+        }
+        else {
+            overrideUserInterfaceStyle = .light
+        }
+        
+        calculateTip(Any.self)
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
     //Everything originates from a user action
     @IBAction func onTap(_ sender: Any) {
         view.endEditing(true)
@@ -30,13 +63,24 @@ class ViewController: UIViewController {
     @IBAction func calculateTip(_ sender: Any) {
         //already attached "Value Changed" event from segmented control
         // ?? operator will assign bill to 0 if casting is unsuccessful
+        let defaults = UserDefaults.standard
+        let value = defaults.bool(forKey: "roundUp")
+        
         let bill = Double(billAmountTextField.text!) ?? 0
         let tipPercentages = [0.15, 0.18, 0.2]
         
-        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
-        let total = bill + tip
+        var tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        var total = bill + tip
         
         //update labels with newly calculated values
+
+        //if you want to round the tip
+        if value {
+            let nextHighestNum = total.rounded(.up)
+            let amountAdded = nextHighestNum - total
+            total = nextHighestNum
+            tip = tip + amountAdded
+        }
         tipPercentageLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
     }
